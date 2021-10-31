1a16d604887de573d5c8b927c34583ac063e9bb8
William Richard <wrichaard@hotmail.com>
2021-10-31 20:49:01 -0300

    updated test

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,5 +1,10 @@
 const { sum } = require('./calculator')
 //shoud sum 2 and 2 and the result must be 4
 it('houd sum 2 and 2 and the result must be 4', () => {
-    expect(sum(2, 2)).toBe(4)
+    expect(sum(2, 2)).toBe(4);
+});
+
+//it('should sum 2 and 2 even if one of them is a string and the result must be 4'
+it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+    expect(sum('2', '2')).toBe(4);
 });
